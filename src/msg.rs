dbc6607bd36f34d7b8307922d719e3838947692d
iricehasan <hsn.irice@gmail.com>
2024-01-09 01:46:02 +0300

    Added messages

---
+++ src/msg.rs
@@ -0,0 +1,22 @@
+use cosmwasm_schema::cw_serde;
+
+#[cw_serde]
+pub struct InstantiateMsg {
+    pub counter_value: u64,
+}
+
+#[cw_serde]
+pub enum ExecuteMsg {
+    Increment {},
+    Reset { value: u64 },
+}
+
+#[cw_serde]
+pub enum QueryMsg {
+    Value {},
+}
+
+#[cw_serde]
+pub struct ValueResp {
+    pub value: u64,
+}
